7915a544387d15a40bc879f00f65463ca2c4dae8
fakhriidjamaris <djamarisfakhri@gmail.com>
2025-05-12 23:58:54 +0700

    update email notifications

--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -1,14 +1,14 @@
 <x-mail::message>
-    # Verifikasi Alamat Email
+# Verifikasi Alamat Email
 
-    Terima kasih telah mendaftar! Silakan klik tombol di bawah untuk memverifikasi alamat email Anda.
+Terima kasih telah mendaftar! Silakan klik tombol di bawah untuk memverifikasi alamat email Anda.
 
-    <x-mail::button :url="$url">
-        Verifikasi Alamat Email
-    </x-mail::button>
+<x-mail::button :url="$url">
+Verifikasi Alamat Email
+</x-mail::button>
 
-    Jika Anda tidak membuat akun, abaikan email ini.
+Jika Anda tidak membuat akun, abaikan email ini.
 
-    Terima kasih,<br>
-    {{ config('app.name') }}
+Terima kasih,<br>
+{{ config('app.name') }}
 </x-mail::message>
